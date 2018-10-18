f9e869d32ad7936f78e992b79515c7e0fcf8c4ae
Anusha <anusha50898@gmail.com>
2018-10-18 16:39:37 +0530

    select page

---
+++ static/javascript/select.js
@@ -0,0 +1,22 @@
+
+$("#insert").click(function() {
+  $( ".d1").toggle();
+})
+;
+
+
+$("#update").click(function() {
+  $( ".d2").toggle();
+})
+;
+
+
+$("#delete").click(function() {
+  $( ".d3").toggle();
+})
+;
+
+$("#view").click(function() {
+  $( ".d4").toggle();
+})
+;
